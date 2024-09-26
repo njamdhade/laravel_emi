af10b1138df01eb3faf01f1773e02b2b86b1362e
njamdhade <njamdhade@gmail.com>
2024-09-26 13:04:34 +0530

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -37,4 +37,8 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
         $this->middleware('auth')->only('logout');
     }
+    public function username()
+    {
+        return 'username';
+    }
 }
